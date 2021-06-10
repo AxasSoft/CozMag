eabf08ae2f02e7abdbc7f42f884c6c8f8d0de5e8
AxasSoft <44229109+AxasSoft@users.noreply.github.com>
2021-06-10 13:57:01 +0300

    #

---
+++ app/src/main/java/ru/mag/cozmag/MainActivity.kt
@@ -0,0 +1,146 @@
+package ru.mag.cozmag
+
+import android.graphics.Bitmap
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window?.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_main)
+
+        //light тема
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        webView = findViewById(R.id.webview)
+        webView.settings.setJavaScriptEnabled(true)
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                findViewById<FrameLayout>(R.id.splash_screen).apply {
+                    visibility= View.GONE
+                }
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        webView
+        val url=resources.getString(R.string.url)
+        webView.loadUrl( url)
+    }*/
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window?.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        val url=resources.getString(R.string.url)
+        webView = findViewById(R.id.webview)
+
+            // Get the web view settings instance
+        val settings = webView.settings;
+
+        // Enable java script in web view
+        settings.javaScriptEnabled = true
+
+        // Enable and setup web view cache
+        settings.setAppCacheEnabled(true)
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
+        settings.setAppCachePath(cacheDir.path)
+
+
+        // Enable zooming in web view
+        settings.setSupportZoom(false)
+        settings.builtInZoomControls = false
+        settings.displayZoomControls = false
+
+
+        // Zoom web view text
+        //settings.textZoom = 125
+
+
+        // Enable disable images in web view
+        settings.blockNetworkImage = false
+        // Whether the WebView should load image resources
+        settings.loadsImagesAutomatically = true
+
+
+        // More web view settings
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            settings.safeBrowsingEnabled = true  // api 26
+        }
+        //settings.pluginState = WebSettings.PluginState.ON
+        settings.useWideViewPort = true
+        settings.loadWithOverviewMode = true
+        settings.javaScriptCanOpenWindowsAutomatically = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            settings.mediaPlaybackRequiresUserGesture = false
+        }
+
+
+        // More optional settings, you can enable it by yourself
+        settings.domStorageEnabled = true
+        settings.setSupportMultipleWindows(true)
+        settings.loadWithOverviewMode = true
+        settings.allowContentAccess = true
+        settings.setGeolocationEnabled(true)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            settings.allowUniversalAccessFromFileURLs = true
+        }
+
+
+        settings.allowFileAccess = true
+
+        // WebView settings
+        webView.fitsSystemWindows = true
+
+        /* if SDK version is greater of 19 then activate hardware acceleration
+        otherwise activate software acceleration  */
+
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
+
+        webView.loadUrl(url)
+
+        // Set web view client
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+                // Page loading started
+                // Do something
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                // Page loading finished
+                // Enable disable back forward button
+                findViewById<FrameLayout>(R.id.splash_screen).apply {
+                    visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            // If web view have back history, then go to the web view back history
+            webView.goBack()
+        }
+    }
+}
